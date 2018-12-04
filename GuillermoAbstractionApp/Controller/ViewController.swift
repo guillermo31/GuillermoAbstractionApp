117b9c8146330f380b4a9c33f719241f0bfe50e3
Guillermo <memo33101@gmail.com>
2018-12-04 08:28:06 -0700

    Created project, organized folders, and added images to assets

---
+++ GuillermoAbstractionApp/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuillermoAbstractionApp
+//
+//  Created by Ramos aguirre, Guillermo on 12/4/18.
+//  Copyright © 2018 ctec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
