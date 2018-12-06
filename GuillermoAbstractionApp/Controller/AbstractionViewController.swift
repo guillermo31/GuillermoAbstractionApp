54dff27d8d9a94b29ab9eab733f0369ffe7b3fa3
Guillermo <memo33101@gmail.com>
2018-12-06 07:57:58 -0700

    Created PageViewController and updated each ViewController with their respective images and descriptions

---
+++ GuillermoAbstractionApp/Controller/AbstractionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AbstractionViewController.swift
+//  GuillermoAbstractionApp
+//
+//  Created by Ramos aguirre, Guillermo on 12/6/18.
+//  Copyright © 2018 ctec. All rights reserved.
+//
+
+import UIKit
+
+class AbstractionViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
