f3acb60556c81a907ceb99408e03f49cf414ed2c
Guillermo <memo33101@gmail.com>
2018-12-06 09:04:04 -0700

    Updated swipe right and left methods, and constraints

--- GuillermoAbstractionApp/Controller/AbstractionViewController.swift
+++ GuillermoAbstractionApp/Controller/AbstractionViewController.swift
@@ -29,6 +29,58 @@ class AbstractionViewController: UIPageViewController, UIPageViewControllerDataS
         return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
     }
     
+    //MARK:- Datasource required methods
+    /// Swipe Left
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+            else
+        {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0
+            else
+        {
+            return orderedAbstractionViews.last
+        }
+        
+        guard orderedAbstractionViews.count > previousIndex
+            else
+        {
+            return nil
+        }
+        
+        return orderedAbstractionViews[previousIndex]
+    }
+    /// Swipe right
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+            else
+        {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex >= 0
+            else
+        {
+            return nil
+        }
+        
+        guard nextIndex < orderedAbstractionViews.count
+            else
+        {
+            return orderedAbstractionViews.first
+        }
+        
+        return orderedAbstractionViews[nextIndex]
+    }
+    
     
     //MARK:- Lifecycle methods
     override func viewDidLoad()
